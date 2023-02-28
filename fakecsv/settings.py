78555872cf00415d485409a78f684760b49c038b
Avhustin Krushchuk <107961305+20avgust17@users.noreply.github.com>
2023-02-28 21:27:43 +0200

    Update settings.py

--- fakecsv/settings.py
+++ fakecsv/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-(k2_(%)j-+-j+7kwgr^+-on_r4v7shq&1wnq@_ca1kni^qeq^^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
